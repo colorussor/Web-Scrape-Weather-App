4de4ef1a3cbd89aea2675e3dd356aa3d1395b4d9
Kelan Lindahl <lindahlk@wit.edu>
2019-04-02 18:45:51 -0400

    added a ton of front end improvements, broke front end too.

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,14 +15,56 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application implements Initializable {
+	@FXML
+	Image dayone;
+	@FXML
+	Image daytwo;
+	@FXML
+	Image daythree;
+	@FXML
+	Image dayfour;
+	@FXML
+	Image dayfive;
+	@FXML
+	Image currentcondo;
+	@FXML
+	Text location;
+	@FXML
+	Text currentday;
+	@FXML
+	Text currentcondit;
+	@FXML
+	Text currenttemp;
+	@FXML
+	Text day1;
+	@FXML
+	Text day2;
+	@FXML
+	Text day3;
+	@FXML
+	Text day4;
+	@FXML
+	Text day5;
+	@FXML
+	Text day1t;
+	@FXML
+	Text day2t;
+	@FXML
+	Text day3t;
+	@FXML
+	Text day4t;
+	@FXML
+	Text day5t;
 	// the "Go" button for the opening fxml file..
 	@FXML
 	Button goButton;
@@ -30,7 +72,9 @@ public class Main extends Application implements Initializable {
 	@FXML
 	TextField zip;
 	@FXML
-	AnchorPane Anchor;
+	AnchorPane AnchorOpen;
+	@FXML
+	AnchorPane AnchorMain;
 	@FXML
 	Pane pane;
 	@FXML
@@ -42,7 +86,7 @@ public class Main extends Application implements Initializable {
 	private String getText() {
 		String zipcode=zip.getText();
 		return zipcode;
-	}
+	 }
 	// This is the opening page. the one where it asks for a zipcode
 	@Override
 	public void start(Stage primaryStage) {
@@ -65,30 +109,30 @@ public class Main extends Application implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// Pretty easy to understand, when you click the gobutton it opens the main application.
-		String text=getText();
+		 String text=getText();
 		goButton.setOnAction(e->{
 			try {
-				if(text.length()<=6 && text.length()>=5) {
+				 if(text.length()<=6 && text.length()>=5) {
 				closeButtonAction();
 				Parent root2=FXMLLoader.load(getClass().getClassLoader().getResource("application/WeatherAppTest.fxml"));
 				Scene scene2=new Scene(root2);
 				Stage stage=new Stage();
 				stage.setScene(scene2);
 				stage.show();
-				}else {
-					System.out.print("bad");
+			 	}else {
+					System.out.print("bad ");
 					System.out.println(text.length());
-				}
+				 }
 			} catch(Exception ex) {
 				ex.printStackTrace();
-			}
+			 }
 		});
 		// Zip is the text field, when the user hits enter it closes the pane and opens the main one. I need to change it so it will only close if a valid zipcode is entered
 		zip.setOnKeyPressed( ex->{
 			try {
-				String ziptest=getText();
+				 String ziptest=getText();
 				if(ex.getCode()==KeyCode.ENTER) {
-					if(ziptest.length()<=6 && ziptest.length()>=5) {
+					 if(ziptest.length()<=6 && ziptest.length()>=5) {
 						closeButtonAction();
 						Parent root2=FXMLLoader.load(getClass().getClassLoader().getResource("application/WeatherAppTest.fxml"));
 						Scene scene2=new Scene(root2);
@@ -98,7 +142,7 @@ public class Main extends Application implements Initializable {
 					}else {
 						System.out.print("bad");
 						System.out.println(ziptest.length());
-					}
+					 }
 				}
 			} catch(Exception exe) {
 				exe.printStackTrace();
