bb8f5a5b70e15e4a84c4f96cb80c37d07543cca8
Kelan Lindah <lindahlk@wit.edu>
2019-04-09 17:28:49 -0400

    Big commit added alot of things, main class now is smaller,
    two controller classes for the other pane

---
+++ src/application/ControllerOpening.java
@@ -0,0 +1,89 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ControllerOpening implements Initializable {
+	// the "Go" button for the opening fxml file..
+	@FXML
+	Button goButton;
+	// Zipcode text field
+	@FXML
+	TextField zip;
+	@FXML
+	AnchorPane AnchorOpen;
+	@FXML
+	// this is a method that will close ANY pane when called
+	private void closeButtonAction(){
+		Stage stage = (Stage) goButton.getScene().getWindow();
+		stage.close();
+	}
+	private String getText() {
+		String zipcode=zip.getText();
+		return zipcode;
+	}
+	
+	public ControllerOpening(){
+		
+	}
+	
+	public void initialize(URL location, ResourceBundle resources) {
+		// Pretty easy to understand, when you click the gobutton it opens the main application.
+		goButton.setOnAction(e->{
+			try {
+				String text=getText();
+				if(text.length()<=6 && text.length()>=5 && text.matches("[0-9]+")) {
+					closeButtonAction();
+					Parent root2=FXMLLoader.load(getClass().getClassLoader().getResource("application/WeatherAppTest.fxml"));
+					Scene scene2=new Scene(root2);
+					Stage stage=new Stage();
+					stage.setTitle("Weather Application");
+					stage.setScene(scene2);
+					stage.show();
+				}else {
+					System.out.print("bad ");
+					System.out.println(text.length());
+				}
+			} catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		});
+		// Zip is the text field, when the user hits enter it closes the pane and opens the main one. I need to change it so it will only close if a valid zipcode is entered
+		zip.setOnKeyPressed( ex->{
+			try {
+				String text=getText();
+				if(ex.getCode()==KeyCode.ENTER) {
+					if(text.length()<=6 && text.length()>=5 && text.matches("[0-9]+")) {
+						closeButtonAction();
+						Parent root2=FXMLLoader.load(getClass().getClassLoader().getResource("application/WeatherAppTest.fxml"));
+						Scene scene2=new Scene(root2);
+						Stage stage=new Stage();
+						stage.setTitle("Weather Application");
+						stage.setScene(scene2);
+						stage.show();
+					}else {
+						System.out.print("bad");
+						System.out.println(text.length());
+					}
+				}
+			} catch(Exception exe) {
+				//a
+				exe.printStackTrace();
+
+			}
+		});
+
+	}
+
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -29,90 +29,32 @@ import javafx.scene.text.Text;
 
 
 public class Main extends Application implements Initializable {
-	// the "Go" button for the opening fxml file..
-	@FXML
-	Button goButton;
-	// Zipcode text field
-	@FXML
-	TextField zip;
-	@FXML
-	AnchorPane AnchorOpen;
-	@FXML
-	// this is a method that will close ANY pane when called
-	private void closeButtonAction(){
-		Stage stage = (Stage) goButton.getScene().getWindow();
-		stage.close();
-	}
-	private String getText() {
-		String zipcode=zip.getText();
-		return zipcode;
-	}
-	// This is the opening page. the one where it asks for a zipcode
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root=FXMLLoader.load(getClass().getClassLoader().getResource("application/Opening.fxml"));
-			Scene scene=new Scene(root);
-			Stage stage1=new Stage();
-			stage1.setTitle("Weather Application");
-			stage1.setScene(scene);
-			stage1.show();
+		    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/Opening.fxml"));
+		    fxmlLoader.load();
+
+			Scene scene=new Scene(fxmlLoader.getRoot());
+			primaryStage.setTitle("Weather Application");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
-
+	
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// Pretty easy to understand, when you click the gobutton it opens the main application.
-		goButton.setOnAction(e->{
-			try {
-				String text=getText();
-				if(text.length()<=6 && text.length()>=5 && text.matches("[0-9]+")) {
-					closeButtonAction();
-					Parent root2=FXMLLoader.load(getClass().getClassLoader().getResource("application/WeatherAppTest.fxml"));
-					Scene scene2=new Scene(root2);
-					Stage stage=new Stage();
-					stage.setTitle("Weather Application");
-					stage.setScene(scene2);
-					stage.show();
-				}else {
-					System.out.print("bad ");
-					System.out.println(text.length());
-				}
-			} catch(Exception ex) {
-				ex.printStackTrace();
-			}
-		});
-		// Zip is the text field, when the user hits enter it closes the pane and opens the main one. I need to change it so it will only close if a valid zipcode is entered
-		zip.setOnKeyPressed( ex->{
-			try {
-				String text=getText();
-				if(ex.getCode()==KeyCode.ENTER) {
-					if(text.length()<=6 && text.length()>=5 && text.matches("[0-9]+")) {
-						closeButtonAction();
-						Parent root2=FXMLLoader.load(getClass().getClassLoader().getResource("application/WeatherAppTest.fxml"));
-						Scene scene2=new Scene(root2);
-						Stage stage=new Stage();
-						stage.setTitle("Weather Application");
-						stage.setScene(scene2);
-						stage.show();
-					}else {
-						System.out.print("bad");
-						System.out.println(text.length());
-					}
-				}
-			} catch(Exception exe) {
-				//a
-				exe.printStackTrace();
-
-			}
-		});
-
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
+
 }
